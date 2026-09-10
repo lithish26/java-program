7a8b9f549763320b0daac0ccf1515950716b854c
lithish26 <lithishprabu26@gmail.com>
2026-09-10 22:13:16 +0530

    Add files via upload

--- Geeks.java
+++ Geeks.java
@@ -1,8 +1,22 @@
-import java.io.*;
 class Geeks {
-	public static void main(String [] args) {
-		System.out.println("GfG!");
-		System.out.println("GFG!");
-		System.out.println("GfG!");
-	}
+    public static void main(String[] args) {
+        int x = 100;
+      
+
+        System.out.printf("Printing simple integer: x = %d%n", x);
+
+
+        System.out.printf("Formatted with precision: PI = %.2f%n", Math.PI);
+
+        float n = 5.2f;
+
+        
+        System.out.printf("Formatted to specific width: n = %.4f%n", n);
+
+        n = 2324435.3f;
+
+        
+        System.out.printf("Formatted to right margin: n = %20.4f%n", n);
+    }
+
 }
---
+++ Geeks1.java
@@ -0,0 +1,16 @@
+public class Geeks1 {
+
+  
+    public static void main(String[] args) {
+      
+        
+        System.err.print("This is an error message using print().\n");
+
+        
+        System.err.println("This is another error message using println().");
+
+        
+        System.err.printf("Error code: %d, Message: %s%n", 404, "Not Found");
+    }
+}
+

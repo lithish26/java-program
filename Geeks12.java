cebb1d882eae0ad3eba0b87992f469079846d1da
lithish26 <lithishprabu26@gmail.com>
2026-09-24 14:48:27 +0530

    Add files via upload

---
+++ Geeks12.java
@@ -0,0 +1,7 @@
+class Geeks12 {
+    public static void main(String[] args) {
+        System.out.print("GFG!");
+        System.out.print("GFG!");
+        System.out.print("GFG!");
+    }
+}
---
+++ Geeks13.java
@@ -0,0 +1,7 @@
+class Geeks13 {
+    public static void main(String[] args) {
+        System.out.println("GFG!");
+        System.out.println("GFG!");
+        System.out.println("GFG!");
+    }
+}
